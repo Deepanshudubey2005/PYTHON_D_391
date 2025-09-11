a1b5c125a73872115ed21cf91298c0c7bb167e33
Deepanshudubey2005 <deepanshudubey66@gmail.com>
2025-09-11 15:05:45 +0530

    While loop

--- commit13.py
+++ commit13.py
@@ -2,5 +2,21 @@ i=1
 while i<6 :
     print(i)
     i+=1
+i=1
+while i<6 :
+    
+    if i ==3 :
+        break
+    print(i)    
+    i+=1  
+j=0
+while j<6 :
+    j+=1
+    if j == 3 :
+        continue
+    
+    print(j)   
+     
+       
 
     
